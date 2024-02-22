b81ac7515dd4356a47566587dc1c50f1da7b47df
hanthinzaraung <hanthinzar1@gmail.com>
2024-02-22 22:49:05 +0630

    use-state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [fruit,setFruit] = useState('apple'); //get //changeFunction
+  let [count,setCount] = useState(0);
+  let changeFruit  = () =>{
+    // fruit = 'orange';
+    // console.log(fruit);
+    setFruit('orange');
+  }
+  let increase= () => {
+    setCount((prevState)  => prevState+1);
+    setCount((prevState)  => prevState+1);
+    setCount((prevState)  => prevState+1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      <h5>{fruit}</h5>
+      <button onClick={changeFruit}>Change Fruit</button>
+      </div>
+      <div>
+        <h1>Counter</h1>
+        <h5>Count - {count}</h5>
+        <button onClick={increase}>Increase</button>
+      </div>
+
+    </>
+    
   );
 }
 
